2fc44a5125ea59ea9484d998041231d69c0c24fc
Алексей Чернышов <Leshac@mail.ru>
2018-08-23 14:25:11 +0300

    Изменение строки подключения для работы с БД на работе

--- testing_program/Data Base/working_with_sql.cs
+++ testing_program/Data Base/working_with_sql.cs
@@ -14,8 +14,9 @@ namespace testing_program
 
     class Working_with_sql
     {
-        const string string_connection = @"Data Source=tcp:93.170.108.82,1433;Initial Catalog=testing_program;Persist Security Info=True;User ID=dba; Password=sql";
+        // const string string_connection = @"Data Source=tcp:93.170.108.82,1433;Initial Catalog=testing_program;Persist Security Info=True;User ID=dba; Password=sql";
         //const string string_connection = @"Data Source=DESKTOP-O70R3EM,1433;Initial Catalog=testing_program;Persist Security Info=True;User ID=dba; Password=sql";
+         const string string_connection = @"Data Source=tcp:ws0151205,1433;Initial Catalog=testing_program;Persist Security Info=True;User ID=dba; Password=sql";
         public SqlConnection connect_to_the_database()
         {
             SqlConnection dbconnect = new SqlConnection(string_connection);
