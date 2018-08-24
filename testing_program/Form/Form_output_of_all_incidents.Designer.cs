39be029c2170f5523ce19d38d7c866043cbcb145
Алексей Чернышов <Leshac@mail.ru>
2018-08-24 09:25:37 +0300

    Программное создание datagridView

--- testing_program/Form/Form_output_of_all_incidents.Designer.cs
+++ testing_program/Form/Form_output_of_all_incidents.Designer.cs
@@ -29,41 +29,8 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_output_of_all_incidents));
-            this.DGV_all_incidents = new System.Windows.Forms.DataGridView();
             this.btn_exit = new System.Windows.Forms.Button();
-            this.FIO = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.DGV_all_incidents)).BeginInit();
             this.SuspendLayout();
-            // 
-            // DGV_all_incidents
-            // 
-            this.DGV_all_incidents.AllowUserToAddRows = false;
-            this.DGV_all_incidents.AllowUserToDeleteRows = false;
-            this.DGV_all_incidents.AllowUserToOrderColumns = true;
-            this.DGV_all_incidents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_all_incidents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.FIO,
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5,
-            this.Column6,
-            this.Column7,
-            this.Column8});
-            this.DGV_all_incidents.Location = new System.Drawing.Point(13, 13);
-            this.DGV_all_incidents.Name = "DGV_all_incidents";
-            this.DGV_all_incidents.ReadOnly = true;
-            this.DGV_all_incidents.Size = new System.Drawing.Size(1018, 407);
-            this.DGV_all_incidents.TabIndex = 0;
             // 
             // btn_exit
             // 
@@ -75,87 +42,20 @@
             this.btn_exit.UseVisualStyleBackColor = true;
             this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
             // 
-            // FIO
-            // 
-            this.FIO.HeaderText = "ФИО";
-            this.FIO.Name = "FIO";
-            this.FIO.ReadOnly = true;
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Тяжесть последствий";
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Вид происшествия";
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Месяц";
-            this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Дата Рождения";
-            this.Column4.Name = "Column4";
-            this.Column4.ReadOnly = true;
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Возраст на момент НС";
-            this.Column5.Name = "Column5";
-            this.Column5.ReadOnly = true;
-            // 
-            // Column6
-            // 
-            this.Column6.HeaderText = "Пол";
-            this.Column6.Name = "Column6";
-            this.Column6.ReadOnly = true;
-            // 
-            // Column7
-            // 
-            this.Column7.HeaderText = "Дата происшествия";
-            this.Column7.Name = "Column7";
-            this.Column7.ReadOnly = true;
-            // 
-            // Column8
-            // 
-            this.Column8.HeaderText = "Время от начала смены";
-            this.Column8.Name = "Column8";
-            this.Column8.ReadOnly = true;
-            // 
             // Form_output_of_all_incidents
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1060, 458);
             this.Controls.Add(this.btn_exit);
-            this.Controls.Add(this.DGV_all_incidents);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form_output_of_all_incidents";
             this.Text = "Инциденты внесенные в программу";
-            ((System.ComponentModel.ISupportInitialize)(this.DGV_all_incidents)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView DGV_all_incidents;
         private System.Windows.Forms.Button btn_exit;
-        private System.Windows.Forms.DataGridViewTextBoxColumn FIO;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
     }
 }
--- testing_program/Form/Form_output_of_all_incidents.cs
+++ testing_program/Form/Form_output_of_all_incidents.cs
@@ -13,26 +13,69 @@ namespace testing_program
 {
     public partial class Form_output_of_all_incidents : Form
     {
+        private DataGridView dgv_all_accident = new DataGridView();
+        const int ColumnCount=9;
         public Form_output_of_all_incidents()
         {
             InitializeComponent();
-            Load_Data();
+            
+            DGV_setup();
+            Populate_dgv();
         }
-        public void Load_Data()
+
+        private void DGV_setup()
+        {
+            this.Controls.Add(dgv_all_accident);
+
+            dgv_all_accident.ColumnCount = ColumnCount;
+            dgv_all_accident.Name = "dgv_all_accident";
+            dgv_all_accident.Location = new Point(8, 8);
+            // dgv_all_accident.Size = new Size(500, 250);
+            dgv_all_accident.AutoSize = true;
+
+
+            dgv_all_accident.Columns[0].Name = "ФИО";
+            dgv_all_accident.Columns[1].Name = "Тяжесть последствий";
+            dgv_all_accident.Columns[2].Name = "Вид происшествия";
+            dgv_all_accident.Columns[3].Name = "Месяц";
+            dgv_all_accident.Columns[4].Name = "Дата рождения";
+            dgv_all_accident.Columns[5].Name = "Возраст на момент НС";
+            dgv_all_accident.Columns[6].Name = "Пол";
+            dgv_all_accident.Columns[7].Name = "Дата происшествия";
+            dgv_all_accident.Columns[8].Name = "Время от начала смены";
+        }
+
+        private void Populate_dgv()
         {
             SqlConnection myConnection; Working_with_sql working_With_Sql = new Working_with_sql();
             myConnection = working_With_Sql.connect_to_the_database();
 
-            string sqlquery = "select CONCAT(People_for_edu.LastName, People_for_edu.Name, People_for_edu.FatherName),seriuosness.Seriousness ,CONCAT(Type_accident.N_type,Type_accident.Name),Acc.month, People_for_edu.Birthday, Acc.Age_on_accident, Sex.Name,Acc.Datetime,Acc.[Time_acc_work(in hours)] from People_for_edu, Acc,Type_accident,seriuosness, Sex where People_for_edu.id=Acc.id_human AND Type_accident.id_type=Acc.id_type AND Acc.id_seriousness=seriuosness.id AND Sex.id=People_for_edu.id_sex";
+            string sqlquery = "select CONCAT(People_for_edu.LastName," +
+                              " People_for_edu.Name, People_for_edu.FatherName)," +
+                              "seriuosness.Seriousness, " +
+                              "CONCAT(Type_accident.N_type,Type_accident.Name)," +
+                              "Acc.month," +
+                              " People_for_edu.Birthday," +
+                              " Acc.Age_on_accident," +
+                              " Sex.Name," +
+                              "Acc.Datetime," +
+                              "Acc.[Time_acc_work(in hours)]" +
+
+                              " from People_for_edu," +
+                              " Acc,Type_accident," +
+                              "seriuosness," +
+                              " Sex " +
+                              "where People_for_edu.id=Acc.id_human AND" +
+                              " Type_accident.id_type=Acc.id_type AND" +
+                              " Acc.id_seriousness=seriuosness.id AND" +
+                              " Sex.id=People_for_edu.id_sex";
             SqlCommand sqlCommand = new SqlCommand(sqlquery, myConnection);
             SqlDataReader reader = sqlCommand.ExecuteReader();
 
             List<string[]> data = new List<string[]>();
-
             while (reader.Read())
             {
-                data.Add(new string[9]);
-
+                data.Add(new string[ColumnCount]);
 
                 data[data.Count - 1][0] = reader[0].ToString();
                 data[data.Count - 1][1] = reader[1].ToString();
@@ -44,7 +87,6 @@ namespace testing_program
                 data[data.Count - 1][6] = reader[6].ToString();
                 data[data.Count - 1][7] = reader[7].ToString();
                 data[data.Count - 1][8] = reader[8].ToString();
-
             }
             reader.Close();
 
@@ -57,10 +99,11 @@ namespace testing_program
 
             foreach (string[] s in data)
             {
-                DGV_all_incidents.Rows.Add(s);
+                dgv_all_accident.Rows.Add(s);
             }
-
         }
+
+        
 
 
         private void btn_exit_Click(object sender, EventArgs e)
