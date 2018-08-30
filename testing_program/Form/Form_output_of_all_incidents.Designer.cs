976d6f3ac699d9c8062a4fc527cfb9aa79f1293d
Алексей Чернышов <Leshac@mail.ru>
2018-08-30 11:55:06 +0300

    Вывод  данныхиз таблицы  всех инцидентов

--- testing_program/Form/Form_output_of_all_incidents.Designer.cs
+++ testing_program/Form/Form_output_of_all_incidents.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // btn_exit
             // 
-            this.btn_exit.Location = new System.Drawing.Point(871, 427);
+            this.btn_exit.Location = new System.Drawing.Point(1049, 551);
             this.btn_exit.Name = "btn_exit";
             this.btn_exit.Size = new System.Drawing.Size(75, 23);
             this.btn_exit.TabIndex = 1;
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1060, 458);
+            this.ClientSize = new System.Drawing.Size(1152, 602);
             this.Controls.Add(this.btn_exit);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form_output_of_all_incidents";
--- testing_program/Form/Form_output_of_all_incidents.cs
+++ testing_program/Form/Form_output_of_all_incidents.cs
@@ -14,7 +14,7 @@ namespace testing_program
     public partial class Form_output_of_all_incidents : Form
     {
         private DataGridView dgv_all_accident = new DataGridView();
-        const int ColumnCount=11;
+        const int ColumnCount=27;
         public Form_output_of_all_incidents()
         {
             InitializeComponent();
@@ -30,21 +30,38 @@ namespace testing_program
             dgv_all_accident.ColumnCount = ColumnCount;
             dgv_all_accident.Name = "dgv_all_accident";
             dgv_all_accident.Location = new Point(8, 8);
-            // dgv_all_accident.Size = new Size(500, 250);
-            dgv_all_accident.AutoSize = true;
+             dgv_all_accident.Size = new Size(1000, 700);
+            //dgv_all_accident.AutoSize = true;
+           // dgv_all_accident.ScrollBars = Both;
 
 
             dgv_all_accident.Columns[0].Name = "ФИО";
             dgv_all_accident.Columns[1].Name = "Тяжесть последствий";
-            dgv_all_accident.Columns[2].Name = "Вид происшествия";
+            dgv_all_accident.Columns[2].Name = "Дата происшествия";
             dgv_all_accident.Columns[3].Name = "Месяц";
-            dgv_all_accident.Columns[4].Name = "Дата рождения";
-            dgv_all_accident.Columns[5].Name = "Возраст на момент НС";
-            dgv_all_accident.Columns[6].Name = "Пол";
-            dgv_all_accident.Columns[7].Name = "Дата происшествия";
-            dgv_all_accident.Columns[8].Name = "Время от начала смены";
-            dgv_all_accident.Columns[9].Name = "Причина происшествия";
-            dgv_all_accident.Columns[10].Name = "Дата начала трудовой деятельности";
+            dgv_all_accident.Columns[4].Name = "Вид происшествия";
+            dgv_all_accident.Columns[5].Name = "Причина";
+            dgv_all_accident.Columns[6].Name = "Дата рождения  ";
+            dgv_all_accident.Columns[7].Name = "Возраст на момент НС";
+            dgv_all_accident.Columns[8].Name = "Пол";
+            dgv_all_accident.Columns[9].Name = "Дата начала труд. Деят.";
+            dgv_all_accident.Columns[10].Name = "Время от начала смены";
+            dgv_all_accident.Columns[11].Name = "День недели НС";
+            dgv_all_accident.Columns[12].Name = "Дата повторного (последнего) инструктажа";
+            dgv_all_accident.Columns[13].Name = "Время после инстр-жа";
+            dgv_all_accident.Columns[14].Name = "Дата проверки знаний";
+            dgv_all_accident.Columns[15].Name = "Время после проверки знаний";
+            dgv_all_accident.Columns[16].Name = "Стажировка";
+            dgv_all_accident.Columns[17].Name = "СИЗ";
+            dgv_all_accident.Columns[18].Name = "УТ";
+            dgv_all_accident.Columns[19].Name = "ОВПФ";
+            dgv_all_accident.Columns[20].Name = "Алкоголь";
+            dgv_all_accident.Columns[21].Name = "Семья";
+            dgv_all_accident.Columns[22].Name = "Образование";
+            dgv_all_accident.Columns[23].Name = "Профессия";
+            dgv_all_accident.Columns[24].Name = "Квалификация";
+            dgv_all_accident.Columns[25].Name = "Предприятие";
+            dgv_all_accident.Columns[26].Name = "ОКВЭД/отрасль";
         }
 
         private void Populate_dgv()
@@ -52,33 +69,86 @@ namespace testing_program
             SqlConnection myConnection; Working_with_sql working_With_Sql = new Working_with_sql();
             myConnection = working_With_Sql.connect_to_the_database();
 
-            string sqlquery = "select CONCAT(People_for_edu.LastName," +
-                              " People_for_edu.Name, People_for_edu.FatherName)," +
-                              "seriuosness.Seriousness, " +
-                              "CONCAT(Type_accident.N_type,Type_accident.Name)," +
-                              "Acc.month," +
-                              " People_for_edu.Birthday," +
-                              " Acc.Age_on_accident," +
-                              " Sex.Name," +
-                              "Acc.Datetime," +
-                              "Acc.[Time_acc_work(in hours)]," +
-                              "CONCAT(Code_reason.N_code, Code_reason.Name),"+
-                              "People_for_edu.Date_start_work "+
+            string sqlquery = "select "+
+"CONCAT(People_for_edu.LastName, People_for_edu.Name, People_for_edu.FatherName),"+
+" seriuosness.Seriousness,"+
+ "acc.Datetime,"+
+ "acc.month,"+
+ "CONCAT(Type_accident.N_type, Type_accident.Name),"+
+ "CONCAT(Code_reason.N_code, Code_reason.Name),"+ 
+ "People_for_edu.Birthday," +
+ "Acc.Age_on_accident," +
+ "Sex.[Name]," +
+"People_for_edu.Date_start_work," +
+"acc.[Time_acc_work(in hours)]," +
+"Acc.Dayweek," +
+"datesInstructions.Date_last_inst," +
+"datesInstructions.Time_after_instr," +
+"datesInstructions.Date_last_check," +
+"datesInstructions.Time_after_instr," +
+"Work.internship," +
+"Acc.SIZ," +
+"CONCAT( SOUT_ARM.APFD, SOUT_ARM.Biological, SOUT_ARM.II,SOUT_ARM.IZ,SOUT_ARM.Khemical,SOUT_ARM.Microclimate,SOUT_ARM.N,SOUT_ARM.NI,SOUT_ARM.Noise,SOUT_ARM.SIZ,SOUT_ARM.SS,SOUT_ARM.T,SOUT_ARM.T,SOUT_ARM.UT,SOUT_ARM.UZ,SOUT_ARM.VL,SOUT_ARM.VO)," +
+"OVPF.[Name]," +
+"Acc.Alcohol," +
+"Family.[Name]," +
+"Education.[Name]," +
+"Profession.[Name]," +
+"Qualification.[Name]," +
+"Enterprise.[Name]," +
+"Enterprise.id_OKVED " +
 
-                              "from People_for_edu," +
-                              "Acc," +
-                              "Type_accident," +
-                              "seriuosness," +
-                              " Sex, " +
-                              "Codes_reason_in_accident, "+
-                              "Code_reason "+
 
-                              "where People_for_edu.id=Acc.id_human AND" +
-                              " Type_accident.id_type=Acc.id_type AND" +
-                              " Acc.id_seriousness=seriuosness.id AND" +
-                              " Sex.id=People_for_edu.id_sex AND"+
-                              " Code_reason.id=Codes_reason_in_accident.id_codes AND"+
-                              " Codes_reason_in_accident.id_accident = Acc.id_accident";
+
+
+
+
+
+
+
+"from People_for_edu left Join Sex on People_for_edu.id_sex = Sex.id " +
+
+                    "left Join Education on People_for_edu.id_education = Education.id" +
+
+                   " left Join Profession on People_for_edu.id_profession = Profession.id " +
+
+                    "left Join Qualification on People_for_edu.id_qualification = Qualification.id " +
+
+                  "  left Join Family on People_for_edu.id_family = Family.id " +
+
+                   " left Join Acc on People_for_edu.id = Acc.id_human " +
+
+                   " left Join seriuosness on seriuosness.id = Acc.id_seriousness " +
+
+                   " left Join Type_accident on Type_accident.id_type = acc.id_type " +
+
+                   " left Join Work_actions on Work_actions.id_work_human = People_for_edu.id " +
+
+                   " left Join Work on Work.id = Work_actions.id_work " +
+
+                   " left Join Enterprise on Enterprise.id = Work.id_enterprise " +
+
+                  "  left Join Shedule on Shedule.id = Work.id_work_schedule " +
+
+                   " left Join Conditions on Conditions.id = Work.id_conditions " +
+
+                    "left Join OVPF on OVPF.id = Work.id_OVPF " +
+
+                   " left Join Info_education on Info_education.id_human = People_for_edu.id " +
+
+                  "  left Join Instruct on Instruct.id = Info_education.id_instruct " +
+
+                   " left Join Type_instruct on Type_instruct.id = Instruct.id_type_instruct " +
+
+                   " left Join Topic_instruct on Topic_instruct.id = Instruct.id_topic_instruct " +
+
+                   " left Join Codes_reason_in_accident on Codes_reason_in_accident.id_accident = acc.id_accident " +
+
+                   " left Join Code_reason on Code_reason.id = Codes_reason_in_accident.id_codes " +
+
+                   " left Join datesInstructions on datesInstructions.id_hum = People_for_edu.id " +
+
+                    "left join SOUT_ARM on SOUT_ARM.id_acc = Acc.id_accident ";  
             SqlCommand sqlCommand = new SqlCommand(sqlquery, myConnection);
             SqlDataReader reader = sqlCommand.ExecuteReader();
 
@@ -99,6 +169,22 @@ namespace testing_program
                 data[data.Count - 1][8] = reader[8].ToString();
                 data[data.Count - 1][9] = reader[9].ToString();
                 data[data.Count - 1][10] = reader[10].ToString();
+                data[data.Count - 1][11] = reader[11].ToString();
+                data[data.Count - 1][12] = reader[12].ToString();
+                data[data.Count - 1][13] = reader[13].ToString();
+                data[data.Count - 1][14] = reader[14].ToString();
+                data[data.Count - 1][15] = reader[15].ToString();
+                data[data.Count - 1][16] = reader[16].ToString();
+                data[data.Count - 1][17] = reader[17].ToString();
+                data[data.Count - 1][18] = reader[18].ToString();
+                data[data.Count - 1][19] = reader[19].ToString();
+                data[data.Count - 1][20] = reader[20].ToString();
+                data[data.Count - 1][21] = reader[21].ToString();
+                data[data.Count - 1][22] = reader[22].ToString();
+                data[data.Count - 1][23] = reader[23].ToString();
+                data[data.Count - 1][24] = reader[24].ToString();
+                data[data.Count - 1][25] = reader[25].ToString();
+                data[data.Count - 1][26] = reader[26].ToString();
             }
             reader.Close();
 
