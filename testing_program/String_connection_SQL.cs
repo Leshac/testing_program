1986df56b9df2ad5d660ef6a803d7e0111747ee9
Алексей Чернышов <Leshac@mail.ru>
2018-09-05 17:58:03 +0300

    Добавление константы строки подключения

---
+++ testing_program/String_connection_SQL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_program
+{
+    static class String_connection_SQL
+    {
+         public const string string_connection_SQL= @"Data Source=tcp:ws0151205,1433;Initial Catalog=testing_program;Persist Security Info=True;User ID=dba; Password=sql";
+        // const string string_connection = @"Data Source=tcp:93.170.108.82,1433;Initial Catalog=testing_program;Persist Security Info=True;User ID=dba; Password=sql";
+    }
+}
