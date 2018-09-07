2fbf0644debbd43b71e0161641709ed63bfc581b
Алексей Чернышов <Leshac@mail.ru>
2018-09-07 14:22:24 +0300

    Добавление стажа работы

--- testing_program/update_DB.cs
+++ testing_program/update_DB.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
+
 namespace testing_program
 {
     class update_DB
@@ -26,7 +28,7 @@ namespace testing_program
                 }
                 else
                 {
-                    insert_Experience_for_people(id_Human);
+                   // insert_Experience_for_people(id_Human);
                     update_Experience(id_Human);
                 }
 
@@ -59,14 +61,24 @@ namespace testing_program
 
         private void update_Experience(int id_Human)
         {
-            string Update_experience = $"UPDATE Experience Set [general (in_days)]=(SELECT DATEDIFF(DAY,(SELECT MIN(Work.Date_enter) FROM Work  Join Work_actions on Work_actions.id_work=work.id),Acc.Datetime) FROM Work_actions JOIN Work on work.id=Work_actions.id_work JOIN Acc on acc.id_human=Work_actions.id_work_human Where Acc.id_human={id_Human}) Where Experience.id_human={id_Human}";
+            /* string Update_experience = $"UPDATE Experience Set [general (in_days)]=(SELECT DATEDIFF(DAY,(SELECT MIN(Work.Date_enter) FROM Work  Join Work_actions on Work_actions.id_work=work.id),Acc.Datetime) FROM Work_actions JOIN Work on work.id=Work_actions.id_work JOIN Acc on acc.id_human=Work_actions.id_work_human Where Acc.id_human={id_Human}) Where Experience.id_human={id_Human}";
 
-            SqlConnection update = new SqlConnection(ConectionSQL_string.sql_string);
-            update.Open();
+             SqlConnection update = new SqlConnection(ConectionSQL_string.sql_string);
+             update.Open();
 
-            SqlCommand update_command = new SqlCommand(Update_experience, update);
-            update_command.ExecuteNonQuery();
-            update.Close();
+             SqlCommand update_command = new SqlCommand(Update_experience, update);
+             update_command.ExecuteNonQuery();
+             update.Close();
+             */
+
+            // получаем таблицу работ на которых работал человек
+            DataTable table_work_for_human= get_table_work_for_human(id_Human);
+            get_general_experience(table_work_for_human);
+
+
+
+
+
         }
 
         private void insert_Experience_for_people(int id_Human)
@@ -79,6 +91,35 @@ namespace testing_program
             insert_command.ExecuteNonQuery();
 
             insert_sql.Close();
+        }
+
+        private DataTable get_table_work_for_human(int id_Human)
+        {
+            string get_id_work_for_human = $"SELECT Work_actions.id_work_human, work.Date_enter, Work.Date_remove FROM Work_actions JOIN Work on Work.id = Work_actions.id_work Where Work_actions.id_work_human = {id_Human} ORDER BY Work.Date_enter ";
+
+            SqlConnection conn = new SqlConnection(ConectionSQL_string.sql_string);
+            conn.Open();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(get_id_work_for_human, conn);
+
+            DataTable table_id_work_for_human = new DataTable();
+            adapter.Fill(table_id_work_for_human);
+            return (table_id_work_for_human);
+        }
+
+        private int get_general_experience(DataTable table_work_for_human)
+        {
+            int experience = 0;
+            int a =table_work_for_human.Rows.Count;
+            for (int i = 0; i <= a-1; i++)
+            {
+                System.TimeSpan exp = Convert.ToDateTime(table_work_for_human.Rows[i][i + 2]).Subtract(Convert.ToDateTime(table_work_for_human.Rows[i][i + 1]));
+                experience = exp.Days+experience;
+                int q = 1;
+            }
+
+
+            return (experience);
         }
     }
 }
